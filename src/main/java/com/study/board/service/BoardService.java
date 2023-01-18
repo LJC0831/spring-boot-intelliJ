c99e69112d9d87c641e2bc6388fd9959ae1a1b52
leejc <leejc831@naver.com>
2023-01-18 12:14:50 +0900

    게시글리스트 불러오기

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -5,11 +5,19 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
     @Autowired
     public BoardRepository boardRepository;
     public void write(Board board){
+
         boardRepository.save(board);
     }
+
+    public List<Board> boardList() {
+        return boardRepository.findAll();
+    }
+
 }
