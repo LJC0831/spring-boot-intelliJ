049d14ebd5dcbd6e9882f84f3e21c17fc93949ca
leejc <leejc831@naver.com>
2023-02-11 22:22:06 +0900

    페이징처리

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -3,6 +3,8 @@ package com.study.board.service;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,8 +30,8 @@ public class BoardService {
         boardRepository.save(board);
     }
 
-    public List<Board> boardList() { //게시글 리스트 처리
-        return boardRepository.findAll();
+    public Page<Board> boardList(Pageable pageable) { //게시글 리스트 처리
+        return boardRepository.findAll(pageable);
     } //게시글 리스트 처리
 
     public Board boardView(Integer id){ //특정 게시글 불러오기
